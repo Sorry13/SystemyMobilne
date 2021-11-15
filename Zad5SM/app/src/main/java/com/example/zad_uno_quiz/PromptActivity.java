a37c80cbc4b3716de3aff6697e164cf0ca82cd26
Sorry13 <88503826+Sorry13@users.noreply.github.com>
2021-11-15 01:44:19 +0100

    Add files via upload

---
+++ Zad5SM/app/src/main/java/com/example/zad_uno_quiz/PromptActivity.java
@@ -0,0 +1,45 @@
+package com.example.zad_uno_quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class PromptActivity extends AppCompatActivity {
+
+    private boolean correctAnswer;
+    private Button showCorrectAnswerButton;
+    private TextView answerTextView;
+    public static final String KEY_EXTRA_ANSWER_SHOWN = "answerShown";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prompt);
+        showCorrectAnswerButton = findViewById(R.id.showCorrectAnswerButton);
+        answerTextView = findViewById(R.id.answerTextView);
+
+
+        correctAnswer = getIntent().getBooleanExtra(MainActivity.KEY_EXTRA_ANSWER, true);
+
+        showCorrectAnswerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int answer = correctAnswer ? R.string.button_true : R.string.button_false;
+                answerTextView.setText(answer);
+                setAnswerShownResult(true);
+
+            }
+        });
+    }
+    private void setAnswerShownResult(boolean answerWasShown) {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(KEY_EXTRA_ANSWER_SHOWN, answerWasShown);
+        setResult(RESULT_OK, resultIntent);
+    }
+}
