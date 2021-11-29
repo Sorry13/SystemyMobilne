8a9e9042d111206d95e3bf527dace9a20b31a4fc
Sorry13 <88503826+Sorry13@users.noreply.github.com>
2021-11-29 20:27:29 +0100

    Add files via upload

---
+++ Zadanie6/app/src/main/java/com/example/Zadanie6/TaskListActivity.java
@@ -0,0 +1,18 @@
+package com.example.Zadanie6;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class TaskListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected Fragment createFragment() {
+        return new TaskListFragment();
+    }
+}
